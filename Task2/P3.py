aab0644a1d423fbd948616d7ba7ac385250ec45c
MoNafea01 <113165914+MoNafea01@users.noreply.github.com>
2023-07-08 05:16:24 +0200

    P3.py

---
+++ Task2/P3.py
@@ -0,0 +1,11 @@
+"""
+Problem 3 🡪 Take from user x number and prints the factorial of
+number X
+"""
+
+nums3 = int(input("Enter number: "))
+print(math.factorial(nums3))
+fac = 1
+for i in range(2, nums3+1):
+    fac *= i
+print(fac)
