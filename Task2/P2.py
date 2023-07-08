253a6b2513241281599c942f5c1c221b973355d7
MoNafea01 <113165914+MoNafea01@users.noreply.github.com>
2023-07-08 05:13:31 +0200

    Ai_Task2

---
+++ Task2/P2.py
@@ -0,0 +1,9 @@
+"""
+Problem 2 🡪 Take from user three numbers A, B, and C, and print them in
+ascending order in one line.
+"""
+
+nums2 = input("Enter numbers separated by space: ").split()
+nums2 = [int(i) for i in nums2]
+nums2.sort()
+print(' '.join(str(e) for e in nums2))
