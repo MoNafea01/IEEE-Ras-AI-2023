d4766682a458bbfee4ae398b68a8c8460e7cab48
MoNafea01 <113165914+MoNafea01@users.noreply.github.com>
2023-08-25 14:52:33 +0200

    IEEE

--- Task 02/P1.py
+++ Task 02/P1.py
@@ -2,6 +2,5 @@
 Take from user N number, Print the summation of the
 numbers that are between 1 and N without using loops.
 """
-
 nums1 = int(input("Enter the number: "))
 print(int(nums1*(nums1+1)/2))
