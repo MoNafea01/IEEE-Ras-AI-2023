efda1f4bdd9346a989debbd487417cefb52fc8f3
MoNafea01 <113165914+MoNafea01@users.noreply.github.com>
2023-08-25 14:52:52 +0200

    IEEE

--- Task 02/P2.py
+++ Task 02/P2.py
@@ -2,7 +2,6 @@
 Take from user three numbers A, B, and C, and print them in
 ascending order in one line.
 """
-
 nums2 = input("Enter numbers separated by space: ").split()
 nums2 = [int(i) for i in nums2]
 nums2.sort()
